be1d6aad024f8c5060e7fcd26c0047420efdead3
abdhou <abdhou@proton.me>
2026-02-01 20:06:46 +0100

    Set up Spring Boot application with web and validation dependencies, parent configuration, and OpenAPI integration.

--- src/main/java/app/carrental/CarRentalApplication.java
+++ src/main/java/app/carrental/CarRentalApplication.java
@@ -1,6 +1,11 @@
 package app.carrental;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class CarRentalApplication {
     static void main(String[] args) {
+        SpringApplication.run(CarRentalApplication.class, args);
     }
 }
